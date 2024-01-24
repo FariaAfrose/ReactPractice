1f7909d1f2cb2b549419c1cfa22d64e334ab77fe
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-01-24 23:18:07 +0600

    Add files via upload

---
+++ Day4/13.UseEffect Hook/src/Home.jsx
@@ -0,0 +1,33 @@
+import React, { useState ,useEffect} from 'react';
+import BlogList from './BlogList';
+
+function Home() {
+  const [blogs, setBlogs] = useState([
+    {
+ title: 'My new Page', body: 'Laboris aliquip eu elit minim laboris excepteur proident duis eu consequat consequat nostrud nisi mollit.', author: 'Aki', id: 1,
+    },
+    {
+ title: 'My new website', body: 'Laboris  elit minim laboris excepteur proident duis eu consequat consequat nostrud nisi mollit.', author: 'kai', id: 2,
+    },
+    {
+ title: 'My new social', body: ' elit minim laboris excepteur proident duis eu consequat consequat nostrud nisi mollit.', author: 'Faria', id: 3,
+    },
+  ]);
+  const handelDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+useEffect(() => {
+  console.log('Use effect run');
+  console.log(blogs);
+});
+  return (
+    <div className="Home">
+      <h2>
+        HomePage
+      </h2>
+      <BlogList blogs={blogs} title="All Blogs!" handelDelete={handelDelete} />
+    </div>
+  );
+}
+export default Home;
