7e0e4628b6bf2dc0011190e356076d19a30d8dbc
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-02-01 15:12:04 +0600

    Update main.jsx

--- Day9/2.Component and Props/src/main.jsx
+++ Day9/2.Component and Props/src/main.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import Clock2 from './Clock';
 
-class Clock {
-  print(locale) {
-      return (
-        <h1>
-          <span>
-            {' '}
-            Hello
-            {' '}
-            {new Date().toLocaleTimeString(locale)}
-          </span>
-        </h1>
-);
-  }
-}
-const ClockCOmponent = new Clock();
 function CLockF({ locale }) {
   return (
     <h1>
@@ -28,11 +14,10 @@ function CLockF({ locale }) {
     </h1>
   );
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CLockF locale="bn-BD" />
-    {ClockCOmponent.print('en-US')}
+    <Clock2 locale="en-US"> United kingdom </Clock2>
   </React.StrictMode>,
 
 );
-export default ClockCOmponent;
