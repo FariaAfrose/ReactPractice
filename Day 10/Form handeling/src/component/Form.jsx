ab46cc59a3f878648dc334dd4d29643c5b0620b6
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-02-02 14:16:10 +0600

    Update Form.jsx

--- Day 10/Form handeling/src/component/Form.jsx
+++ Day 10/Form handeling/src/component/Form.jsx
@@ -5,6 +5,8 @@ class Button extends React.Component {
     state = {
         title: 'javaScript',
         text: 'Javascript is awsome',
+        libary: 'React',
+        isAwsome: true,
     };
 
     handelChange = (e) => {
@@ -17,23 +19,54 @@ if (e.target.type === 'text') {
     this.setState({
         text: e.target.value,
     });
+} else if (e.target.type === 'select-one') {
+    this.setState({
+        libary: e.target.value,
+    });
+} else if (e.target.type === 'checkbox') {
+    this.setState({
+        isAwsome: e.target.checked,
+    });
 } else {
     console.log('nothing here');
 }
 };
 
+submitHandel = (e) => {
+    const {
+        title, text, libary, isAwsome,
+       } = this.state;
+    e.preventDefault();
+    console.log(title, text, libary, isAwsome);
+};
+
 render() {
-const { title, text } = this.state;
+const {
+ title, text, libary, isAwsome,
+} = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.submitHandel}>
           <input type="text" placeholder="Enter title" value={title} onChange={this.handelChange} />
+          {/* <p>{title}</p> */}
+          <br />
+          <br />
+          <textarea name="text" value={text} onChange={this.handelChange} />
+          <br />
+          <br />
+          <select value={libary} onChange={this.handelChange}>
+            <option value="React">React</option>
+            <option value="Angular">Angular</option>
+          </select>
+          <br />
+          <br />
+          <input type="checkbox" checked={isAwsome} onChange={this.handelChange} />
+          <br />
+          <br />
+          <input type="submit" value="submit" />
         </form>
-        {/* <p>{title}</p> */}
-        <br />
-        <br />
-        <textarea name="text" value={text} onChange={this.handelChange} />
+
       </div>
     );
 }
