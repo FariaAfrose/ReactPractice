e84d404e267ce1173bb3ccae429aa6c905f3a1e6
Faria Afrose <154304357+FariaAfrose@users.noreply.github.com>
2024-02-04 20:15:30 +0600

    Delete Day11/React Lifting State Up directory

--- Day11/React Lifting State Up/src/component/Boiling.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Boiling({ celsius = 0 }) {
-    if (celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-        return <p> Water would not boil.</p>;
-}
